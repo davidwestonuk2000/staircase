aae16e884bbe0c6baccade1e93bda8c28ad3dead
David Weston <david.weston@office.pancentric.net>
2018-09-26 16:53:45 +0100

    no message

--- src/test/java/StairsTest.java
+++ src/test/java/StairsTest.java
@@ -1,5 +1,9 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
@@ -29,10 +33,23 @@ public class StairsTest extends BaseStaircaseTest {
     }
 
     @Test
+    public void numberOfWaysSequence() {
+        String inputList = "1,2,3,4,5,6,7,8,9,10,11";
+        String outputList = "1,2,3,5,8,13,21,34,55,89,144";
+        List<String> input = Arrays.asList(inputList.split("\\s*,\\s*"));
+        List<String> expectedOutput = Arrays.asList(outputList.split("\\s*,\\s*"));
+
+        // for each item in the input list check the expected result
+        for (int i = 0; i < input.size(); i++) {
+            given().when().get("/stairs/"+input.get(i)).then()
+                  .body(containsString(expectedOutput.get(i)));
+        }
+    }
+
+    @Test
     public void invalidNumberOfStairs() {
         given().when().get("/stairs/abc")
                 .then().statusCode(404);
     }
-
 
 }
