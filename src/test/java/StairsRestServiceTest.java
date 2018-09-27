c06ef4c3ecc5338d18e6849ef00466f3d5aa62ae
David Weston <david.weston@office.pancentric.net>
2018-09-27 08:52:08 +0100

    Adding service response time test

--- src/test/java/StairsRestServiceTest.java
+++ src/test/java/StairsRestServiceTest.java
@@ -1,11 +1,15 @@
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
 
 public class StairsRestServiceTest extends BaseStaircaseTest {
 
@@ -39,6 +43,16 @@ public class StairsRestServiceTest extends BaseStaircaseTest {
     public void invalidNumberOfStairs() {
         given().when().get("/stairs/abc")
                 .then().statusCode(404);
+    }
+
+    // verify service response time
+    @Test
+    public void whenMeasureResponseTime_thenOK() {
+        Response response = RestAssured.get("/stairs/10");
+        long timeInMS = response.time();
+        long timeInS = response.timeIn(TimeUnit.SECONDS);
+
+        assertEquals(timeInS, timeInMS/1000);
     }
 
 }
